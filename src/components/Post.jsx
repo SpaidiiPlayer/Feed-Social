998376b9915f8386b3c0707a2cf5c87fcf91e67a
Matheus Vinagre <matheus.vinagre@hotmail.com>
2023-12-01 13:28:25 -0300

    Fixing bugs in sidebar, and creating posts

---
+++ src/components/Post.jsx
@@ -0,0 +1,37 @@
+import style from './Post.module.css';
+
+export function Post(props) {
+    return (
+        <article className={style.post}>
+            <header>
+                <div className={style.author}>
+                    <img className={style.avatar} src="https://github.com/SpaidiiPlayer.png" alt="" />
+
+                    <div className={style.authorinfo}> 
+                        <strong>Matheus Vinagre</strong>
+                        <span>Web developer</span>
+                    </div>
+                </div>
+
+                <time title='01 de Dezembro de 2023 às 12:13' dateTime="2023-12-01 12:13:30">Publicado há 1h</time>
+            </header>
+            <div className={style.content}>
+                <p>Fala galeraa 👋</p>
+
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                <p>👉 <a href="#">jane.design/doctorcare</a></p>
+
+                <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+                
+            </div>
+
+            <footer>
+                <strong>Deixe seu feedback</strong>
+                <input type="text" />
+                <button></button>
+            </footer>
+
+        </article>
+    )
+}
